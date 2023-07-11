8c04e20e28beeaff1df137db684edab0c1a95262
tecni2 <tecni2.elie.elie@gmail.com>
2023-07-10 23:57:32 -0500

    cambiando imagen About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,24 @@
-import Perfil from "../assets/eleazar_chusmita_3.png";
+import Perfil from "../assets/eleazar_chusmita_1.png";
 
 const About = () => {
   return (
     <div className="w-full flex flex-col lg:flex-row px-8 md:px-10 gap-10 lg:gap-20 lg:py-20">
-
       <div className="w-full md:h-[290px] lg:w-1/3 flex flex-col items-center border border-gray-500 dark:bg-transparent rounded-md">
-        <img src={Perfil} className="h-[290px] p-1 rounded-md ease-in-out duration-300 hover:scale-125" alt="" />
-      
+        <img
+          src={Perfil}
+          className="h-[290px] p-1 rounded-md ease-in-out duration-300 hover:scale-125"
+          alt=""
+        />
       </div>
       <div className="w-full flex flex-col">
-      <p className="text-3xl font-bold text-black dark:text-white"
-      >
-        About Me
-        {/* 11:33 verificar pantalla*/}
-      </p>
+        <p className="text-3xl font-bold text-black dark:text-white">
+          About Me
+        </p>
+        <p>
+          Full.Stack Web Developer with ability to learn and collaborate in rapidly
+        </p>
       </div>
-      
+      {/* 11:33 verificar pantalla*/}
     </div>
   );
 };
