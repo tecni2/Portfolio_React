a2011ac8baa9fc3e54afea45fb3d4c5e8b686898
tecni2 <tecni2.elie.elie@gmail.com>
2023-07-17 01:58:25 -0500

    Add Styles Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,13 @@
 const Skills = () => {
   return (
     <div className="w-full flex flex-col py-20">
-      <h4 className="text-3xl font-bold text-black dark:text-gray-100 mb-10 text-center">Tachnical Skills</h4>
+      <h4 className="text-3xl font-bold text-black dark:text-gray-100 mb-10 text-center">
+        Tachnical Skills
+      </h4>
+
+      <div className="w-full flex flex-wrap gap-10 items-center justify-center">
+        {/* 19:40 */}
+      </div>
     </div>
   );
 };
