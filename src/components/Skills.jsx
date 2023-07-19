6caeead4d371b87aa2977f95a713630022811a78
tecni2 <tecni2.elie.elie@gmail.com>
2023-07-18 23:58:22 -0500

    Change Components Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,20 @@
+// import { Skills } from "../data";
+
 const Skills = () => {
   return (
     <div className="w-full flex flex-col py-20">
       <h4 className="text-3xl font-bold text-black dark:text-gray-100 mb-10 text-center">
-        Tachnical Skills
+        Technical Skills
       </h4>
 
       <div className="w-full flex flex-wrap gap-10 items-center justify-center">
+        {/* {Skills.map((skil, index) => (
+          <div key={index} className="">
+            <div className="">
+              <img src={skil.icon} alt="" />
+            </div>
+          </div>
+        ))} */}
         {/* 19:40 */}
       </div>
     </div>
