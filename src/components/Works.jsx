550016ace1abd5c557c6435017c86fc4d7ebd4f7
tecni2 <tecni2.elie.elie@gmail.com>
2023-07-26 00:00:42 -0500

    Add Componet Works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,1 +1,15 @@
+const Works = () => {
+  return (
+    <div className="w-full flex flex-col py-20 px-8 md:px-10 gap-10 lg:gap-20 lg:py-20">
+      <h4 className="text-3xl font-bold text-black dark:text-gray-100 mt-10">
+        Projects
+      </h4>
+      
+    </div>
+  );
+};
+
+export default Works;
+
+
 // 33:21
