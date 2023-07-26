2a52d1811fbd694d1c7a4c63006e12e506487330
tecni2 <tecni2.elie.elie@gmail.com>
2023-07-26 00:12:39 -0500

    Change Component Works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,15 +1,19 @@
+import { projects } from "../data";
+
 const Works = () => {
   return (
     <div className="w-full flex flex-col py-20 px-8 md:px-10 gap-10 lg:gap-20 lg:py-20">
       <h4 className="text-3xl font-bold text-black dark:text-gray-100 mt-10">
         Projects
       </h4>
-      
+      <div className="flex flex-wrap gap-10 lg:gap-20 justify-center"
+      >
+        <h3>Sección en progreso</h3>
+      </div>
     </div>
   );
 };
 
 export default Works;
-
 
 // 33:21
