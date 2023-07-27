d81707754543252f78bdbf66f623de2f4af06435
tecni2 <tecni2.elie.elie@gmail.com>
2023-07-27 01:40:03 -0500

    Add project imagen and name

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,9 +6,26 @@ const Works = () => {
       <h4 className="text-3xl font-bold text-black dark:text-gray-100 mt-10">
         Projects
       </h4>
-      <div className="flex flex-wrap gap-10 lg:gap-20 justify-center"
-      >
-        <h3>Sección en progreso</h3>
+      <div className="flex flex-wrap gap-10 lg:gap-20 justify-center">
+        {projects.map((p, index) => (
+          <div
+            key={index}
+            className="w-[350px] h-[350] cursor-pointer shadow-xl hover:scale-110 rounded-md"
+          >
+            <img
+              src={p.img}
+              alt={p.title}
+              className="w-full h-[250px] object-cover rounded-md"
+            />
+            <div className="w-full h-[100px] bg-gray-200 dark:bg-[#04133e]">
+              
+              <h4 className="text-2xl text-black dark:text-gray-300 font-semibold py-2 px-1">
+                {p.title}
+              </h4>
+              <p className="text-sm text-orange-600 px-1 uppercase">{p.cat}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
@@ -16,4 +33,4 @@ const Works = () => {
 
 export default Works;
 
-// 33:21
+// 32:05
