c8bc9c42a46e3d016de96532e1b544e02dabcd31
tecni2 <tecni2.elie.elie@gmail.com>
2023-09-10 23:16:12 -0500

    modified icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
       <div className="w-full flex flex-col md:flex-row py-20 px-8 md:px-10 gap-10 lg:gap-20 justify-between">
         <p className="text-lg text-gray-300 font-semibold">
-          Country Venezuela - Colombia
+          Country Colombia
         </p>
         <div className="flex flex-col md:flex-row text-md gap-5 md:gap-20 text-gray-300">
           <a href="#home">Home</a>
@@ -25,8 +25,21 @@ const Footer = () => {
         </p>
 
         <div className="flex gap-10 text-gray-300 text-2xl mb-10">
+
+        <a
+            href="https://github.com/tecni2"
+            target="_blank"
+            className="cursor-pointer"
+          >
           <BsGithub className="hover:scale-125" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/eleazarchusmita/"
+            target="_blank"
+            className="cursor-pointer"
+          >
           <BsLinkedin className="hover:scale-125" />
+          </a>
         </div>
         <p className="text-gray-400 text-md tracking-wider">@2023 Eleazar Chusmita</p>
       </div>
