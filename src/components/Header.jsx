6e94367557429c31f7eacb029851a6768de33647
tecni2 <tecni2.elie.elie@gmail.com>
2023-09-11 00:37:55 -0500

    modified svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,10 +42,10 @@ const Header = () => {
           </a>
         </div>
       </div>
-      <div className="w-[320px] h-[320px] md:w-[600px] md:h-[600px] 2xl:w-[700px] 2xl:h-[700px] rounded-full border border-gray-600 dark:border-gray-200 relative md:nt-20 lg:mt-0 flex items-center justify-center">
+      <div className="w-[320px] h-[320px] md:w-[470px] md:h-[470px] 2xl:w-[600px] 2xl:h-[520px] rounded-full border border-gray-600 dark:border-gray-200 relative md:mt-20 lg:mt-0 flex items-center justify-center">
         <img
           src={Javascript}
-          className="w-40 h-full absolute -top-0 md:-top-10"
+          className="w-40 h-full absolute -top-0 md:-top-0"
           data-aos="fade-down"
           data-aos-offset="200"
           data-aos-delay="50"
@@ -61,22 +61,22 @@ const Header = () => {
          data-aos-easing="ease-in-out">
           <img
             src={Reactjs}
-            className="absolute top-7 md:top-20 left-3 2xl:left-8 w-12 h-12 md:w-20 md:h-20"
+            className="absolute top-7 md:top-20 left-0 2xl:left-8 w-12 h-12 md:w-20 md:h-20"
             alt=""
           />
           <img
             src={Nodejs}
-            className="absolute bottom-7 md:bottom-20 left-3 2xl:left-12 w-12 h-12 md:w-20 md:h-20"
+            className="absolute bottom-7 md:bottom-20 left-1 md:left-1 2xl:left-12 w-12 h-12 md:w-20 md:h-20"
             alt=""
           />
           <img
             src={Nextjs}
-            className="absolute top-3 md:top-20 right-6 2xl:right-7 w-12 h-12 md:w-20 md:h-20"
+            className="absolute top-7 md:top-20 right-5 md:right-0 2xl:right-7 w-12 h-12 md:w-20 md:h-20"
             alt=""
           />
           <img
             src={Redux}
-            className="absolute bottom-7 md:bottom-20 right-3 2xl:right-12 w-12 h-12 md:w-20 md:h-20"
+            className="absolute bottom-7 md:bottom-20 right-1 2xl:right-12 w-12 h-12 md:w-20 md:h-20"
             alt=""
           />
         </div>
