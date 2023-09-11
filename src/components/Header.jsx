c878cd4345fef865a67e4949b319bc2018f95518
tecni2 <tecni2.elie.elie@gmail.com>
2023-09-10 21:54:52 -0500

    correcting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         </span>
         <div className="flex gap-4 items-center my-4">
           <h1 className="text-gray-800 dark:text-neutral-200 text-5xl 2xl:text-7xl font-bold text-center">
-            I'm a
+            I'm
           </h1>
           <div className="flex items-center justify-center text-white shadow-lg bg-blue-800 dark:bg-[#224cff10] dark:text-[#224cff] rounded-full">
             <p className="text-3xl 2xl:text-5xl fond-bold px-5 py-2 pb-2 text-center">
